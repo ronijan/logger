10f29a9a8b162292ca0c1f6629353e6fbb5d8da8
Huseyin <7ussein.5alil@gmail.com>
2020-08-21 12:35:37 +0200

    Update Log.php

--- src/Log.php
+++ src/Log.php
@@ -6,41 +6,26 @@ use DateTime;
 
 class Log
 {
-  /**
-  * @param string|mixed|array|object $data
-  * @return bool
-  */
-  public static function info($data)
-  {
-    $path = __DIR__ . '/../../../../storage/logs/';
+    /**
+     * @param mixed $data
+     */
+    public static function info($data)
+    {
+        $dateTime = new DateTime();
+        $path = __DIR__ . '/../../../../storage/logs/';
 
-    if (! is_dir($path)) {
-      mkdir($path, 0777, true);
+        if (!is_dir($path)) {
+            mkdir($path, 0777, true);
+        }
+
+        $formatData = '# ' . $dateTime->format('Y-m-d H:i:s') . " \t " . serialize($data) . "\r\n";
+
+        $file = $path . 'Log-' . date('m-d-yy') . '-' . md5(date("d")) . '.log';
+
+        if (!file_put_contents($file, $formatData, FILE_APPEND | LOCK_EX)) {
+            die('Failed to create Log file.');
+        }
+
+        file_put_contents($path . '.gitignore', '*.log');
     }
-
-    if (!is_array($data)) {
-      $formatData = self::formattedData($data);
-    }else {
-      foreach ($data as $value) {
-        $formatData = self::formattedData($value);
-      }
-    }
-
-    $file = $path . 'Log-' . date('m-d-yy') . '-' . md5(date("d")) . '.log';
-
-    if (! file_put_contents($file, $formatData, FILE_APPEND | LOCK_EX)) {
-      die('Failed to create Log file.');
-    }
-  }
-
-  /**
-  * @param string|mixed|array|object $data
-  * @return string
-  */
-  public static function formattedData($data)
-  {
-    $dateTime = new DateTime();
-
-    return '# ' . $dateTime->format('Y-m-d H:i:s') . "\t" . $data . "\r\n";
-  }
 }
