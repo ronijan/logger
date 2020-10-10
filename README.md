de21d1ea82ca9f03209afaba86744c84a8ad804d
Huseyin <7ussein.5alil@gmail.com>
2020-10-10 14:28:59 -0400

    code reformat

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 ### Logger
 
 Composer install $ `composer require ronijan/logger`
+
+```php
+
+use Ronijan\Logger\Log;
+
+$list = [
+    'id' => 1,
+    'name' => 'john doe',
+    'url' => 'www.example.com'
+];
+
+Log::info($list); 
+
+```
--- src/Log.php
+++ src/Log.php
@@ -6,20 +6,16 @@ use DateTime;
 
 class Log
 {
-    /**
-     * @param mixed $data
-     */
-    public static function info($data)
+    public static function info($data): void
     {
         $dateTime = new DateTime();
         $path = __DIR__ . '/../../../../storage/logs/';
 
-        if (!is_dir($path)) {
-            mkdir($path, 0777, true);
+        if (!mkdir($path, 0777, true) && !is_dir($path)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $path));
         }
 
         $formatData = '# ' . $dateTime->format('Y-m-d H:i:s') . " \t " . serialize($data) . "\r\n";
-
         $file = $path . 'Log-' . date('m-d-yy') . '-' . md5(date("d")) . '.log';
 
         if (!file_put_contents($file, $formatData, FILE_APPEND | LOCK_EX)) {
