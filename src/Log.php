6dd3d1f0da9df7f453986882166812b71b100755
Huseyin <7ussein.5alil@gmail.com>
2021-01-08 10:28:24 +0100

    Update Log.php

--- src/Log.php
+++ src/Log.php
@@ -8,60 +8,59 @@ class Log
 {
     public const DIR = __DIR__ . '/../../../../storage/logs/';
 
-    public function __construct()
-    {
-        $this->gitignore();
-    }
-
     public static function info($data): void
     {
+        self::makeDir();
         $file = 'Info-' . date('m.Y') . '-' . md5(date("m")) . '.log';
-        self::createFile($file);
+
+        if (!is_file($file)) {
+            file_put_contents($file, '');
+        }
 
         $date = date('d.m.Y h:i:s');
-        $log = '# ' . $date . "\t" . print_r($data, true) . "\n";
+        $log = '# ' . $date . " \t" . print_r($data, true) . "\n";
         error_log($log, 3, self::DIR . $file);
     }
 
     public static function debug($data): void
     {
+        self::makeDir();
         $file = 'Debug-' . date('m.Y') . '-' . md5(date("m")) . '.log';
-        self::createFile($file);
+
+        if (!is_file($file)) {
+            file_put_contents($file, '');
+        }
 
         $date = date('d.m.Y h:i:s');
-        $log = '# ' . $date . "\t" . print_r($data, true) . "\n";
+        $log = '# ' . $date . " \t" . print_r($data, true) . "\n";
         error_log($log, 3, self::DIR . $file);
     }
 
     public static function emergency($data): void
     {
+        self::makeDir();
         $file = 'Emergency-' . date('m.Y') . '-' . md5(date("m")) . '.log';
-        self::createFile($file);
+
+        if (!is_file($file)) {
+            file_put_contents($file, '');
+        }
 
         $date = date('d.m.Y h:i:s');
-        $log = '# ' . $date . "\t" . print_r($data, true) . "\n";
+        $log = '# ' . $date . " \t" . print_r($data, true) . "\n";
         error_log($log, 3, self::DIR . $file);
     }
 
-    private function gitignore(): void
+    private static function makeDir(): void
     {
         if (!file_exists(self::DIR) && !mkdir($concurrentDirectory = self::DIR, 0777,
                 true) && !is_dir($concurrentDirectory)) {
-            throw new RuntimeException(sprintf('Directory "%s" was not created', $concurrentDirectory));
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $concurrentDirectory));
         }
 
         $file = self::DIR . '.gitignore';
-        self::createFile($file);
 
         if (!is_file($file)) {
             file_put_contents($file, '*.log');
-        }
-    }
-
-    private static function createFile(string $file): void
-    {
-        if (!is_file($file)) {
-            file_put_contents($file, '');
         }
     }
 }
